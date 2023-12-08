d29df4df54f1d5bfed293834f0188043a4f47de9
João Vitor <joaovitoroliveirasoarespereira@gmail.com>
2023-12-08 11:38:59 -0300

    Arquivo sobre operadores matematicos e logicos

---
+++ operators/src/main/Operators.java
@@ -0,0 +1,33 @@
+package main;
+
+public class Operators {
+	/*Operador para realizar as operação aritmética de soma ( + )*/
+	int numSoma1 = 2;
+	int numSoma2 = 3;
+	int soma = numSoma1 + numSoma2;
+	
+	/*Operador para realizar a operação aritmética de subtração ( - )*/
+	int numSub1 = 20;
+	int numSub2 = 10;
+	int subtracao = numSub1 - numSub2;	
+	
+	/*Operador para realizar a operação aritmética de multiplicação ( * )*/
+	int numMult1 = 2;
+	int numMult2 = 10;
+	int multiplicacao = numMult1 * numMult2;
+	
+	/*Operador para realizar a operação aritmética de divisão ( / )*/
+	int numDiv1 = 10;
+	int numDiv2 = 2;
+	int divisao = numDiv1 / numDiv2;
+	
+	/* Os operadores lógicos são operadores que retornam um dado booleano ( verdadeiro ou falso )
+	 * para uma comparação entre 2 ou mais valores*/
+	boolean maiorQue = 5 > 2; // Retorna 'true'
+	boolean menorQue = 5 < 2; // Retorna 'false'
+	boolean maiorOuIgual = 5 >= 2; // Retorna 'true'
+	boolean menorOuIgual = 5 <= 2; // Retorna 'false'
+	boolean iguais = 5 == 2; // Retorna 'false'
+	boolean diferentes = 5 != 2; // Retorna 'true'
+	
+}
