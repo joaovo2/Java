f7f1aadc78a113a6e3d010069550953cf0e350fb
João Vitor <joaovitoroliveirasoarespereira@gmail.com>
2023-12-09 00:52:58 -0300

    Arquivo sobre a clausula for loop

---
+++ forLoop/src/main/ForLoop.java
@@ -0,0 +1,13 @@
+package main;
+
+public class ForLoop {
+	public static void main(String args[]) {
+		/* A estrutura For serve para executar uma determinada rotina 
+		 * diversas vezes. Diferentemente da estrutura While, esta estrutura 
+		 * serve para realizar tarefas que tem quantidade determinada para 
+		 * serem repetidas. */
+		for(int contador = 0 ; contador <= 10 ; contador++) {
+			System.out.println("** " + contador + " **");
+		}
+	}
+}
