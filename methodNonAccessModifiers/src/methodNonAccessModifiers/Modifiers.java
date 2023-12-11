f394d5284a4a74bf1c0b354f80f0ff0fe0652adb
João Vitor <joaovitoroliveirasoarespereira@gmail.com>
2023-12-11 20:01:23 -0300

    Arquivo sobre modificadores de não acesso para métodos e atributos

---
+++ methodNonAccessModifiers/src/methodNonAccessModifiers/Modifiers.java
@@ -0,0 +1,27 @@
+package methodNonAccessModifiers;
+
+public class Modifiers {
+	/* Métodos e atributos podem ser restritos por meios de 
+	 * modificadores de não acesso.
+	 * 
+	 *  Métodos e atributos definidos como 'final' não podem 
+	 *  ser sobrescritos ou modificados. 
+	 *  
+	 * Métodos e atributos definidos como 'static' são métodos que 
+	 * pertencem a classe e não ao objeto. Estes métodos podem ser 
+	 * usados chamando direto pelo nome da classe. 
+	 * 
+	 *  Métodos definidos como 'abstract' só podem ser usados em classes 
+	 *  abstratas. Métodos abstratos não possuem corpo. O corpo do método 
+	 *  é definido pela classe que herda a classe abstrata.
+	 *  
+	 *  Métodos definidos como 'transient', atributos e métodos são  
+	 *  ignorados ao serializar o objeto que os contém. 
+	 *  
+	 * Métodos definidos como 'sinchronized' só podem ser acessados 
+	 * por um thread por vez. 
+	 * 
+	 * Atributos definidos como 'volatile' não são armazenado em cache 
+	 * localmente e é sempre lido na "memória principal".
+​*/
+}
